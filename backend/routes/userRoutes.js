28f0efff80af9e508df9fcb9711d1333dda46a17
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-10-19 06:43:44 -0400

    completed tests for POST /user input validation

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,12 +6,32 @@ router.get('/', (req, res, next) => {
   res.sendStatus(418);
 });
 
-router.post('/', body('email').notEmpty().isEmail(), (req, res, next) => {
-  const result = validationResult(req);
-  if (result.isEmpty()) {
-    return res.sendStatus(200);
-  }
-  res.status(400).json(result.errors);
-});
+// create a new user
+router.post(
+  '/',
+  body('email').notEmpty().isEmail().escape(),
+  body('password').notEmpty().isLength({ min: 5, max: 20 }).escape(),
+  body('passwordConf').notEmpty().isLength({ min: 5, max: 20 }).escape(),
+  (req, res, next) => {
+    const result = validationResult(req);
+    const { email, password, passwordConf } = req.body;
+
+    if (password !== passwordConf) {
+      result.errors.push({
+        type: 'field',
+        value: `${passwordConf}`,
+        msg: 'Confirmation password does not match the password.',
+        path: 'passwordConf',
+        location: 'body',
+      });
+    }
+
+    if (result.isEmpty()) {
+      return res.sendStatus(200);
+    }
+
+    res.status(400).json(result.errors);
+  },
+);
 
 module.exports = router;
--- backend/routes/userRoutes.test.js
+++ backend/routes/userRoutes.test.js
@@ -8,24 +8,54 @@ const goodUser = {
   passwordConf: 'sparkle47',
 };
 
-const badUser = {
+const badEmailUser = {
   email: 'unicorn_princess',
+  password: 'xxxxx',
+  passwordConf: 'xxxxxx',
+};
+
+const badPwUser = {
+  email: 'unicorn_princess@email.com',
   password: 'xxx',
-  passwordConf: 'xxxx',
+  passwordConf: 'xxx',
+};
+
+const badPwConfUser = {
+  email: 'unicorn_princess@email.com',
+  password: 'xxxxx',
+  passwordConf: 'xxxxxxx',
 };
 
 describe('POST /user', () => {
   describe('invalid form data', () => {
-    it('returns an error if email is invalid', async () => {
+    it('returns a 400 if email is invalid', async () => {
       const res = await request(app)
         .post('/user')
-        .send(badUser)
+        .send(badEmailUser)
         .set('Accept', 'application/json');
       expect(res.status).toBe(400);
       expect(res.body).toBeInstanceOf(Array);
       expect(res.body.length).toBeGreaterThan(0);
     });
-    // it('returns an error if password is invalid', () => {});
-    // it('returns an error if passwordConf is invalid', () => {});
+
+    it('returns a 400 if password is invalid', async () => {
+      const res = await request(app)
+        .post('/user')
+        .send(badPwUser)
+        .set('Accept', 'application/json');
+      expect(res.status).toBe(400);
+      expect(res.body).toBeInstanceOf(Array);
+      expect(res.body.length).toBeGreaterThan(0);
+    });
+
+    it('returns a 400 if passwordConf is invalid', async () => {
+      const res = await request(app)
+        .post('/user')
+        .send(badPwConfUser)
+        .set('Accept', 'application/json');
+      expect(res.status).toBe(400);
+      expect(res.body).toBeInstanceOf(Array);
+      expect(res.body.length).toBeGreaterThan(0);
+    });
   });
 });
