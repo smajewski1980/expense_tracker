ad4e75e89c3eb316bd381a8387e729830bc7371c
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-10-26 14:03:48 -0400

    corrected the way i was closing the pool connection after the tests

--- backend/routes/expenseRoutes.test.js
+++ backend/routes/expenseRoutes.test.js
@@ -16,154 +16,158 @@ const {
 } = require('../expenseTestResources');
 const { testLoginUser } = require('../userTestResources');
 
-describe('check error handling for db calls', () => {
-  let poolSpy;
-
-  beforeEach(() => {
-    poolSpy = jest.spyOn(pool, 'query');
-  });
-
-  afterEach(() => {
-    poolSpy.mockRestore();
-  });
-
-  it('POST /expense returns 500 if theres a prob connecting to db', async () => {
-    const res = await request(app)
-      .post('/user/login')
-      .send(testLoginUser)
-      .expect(200);
-    const cookie = res.headers['set-cookie'];
-
-    poolSpy.mockImplementation(() => {
-      throw new Error('PostgreSQL database error: Connection refused');
-    });
-
-    await request(app)
-      .post('/expense')
-      .set('Cookie', cookie)
-      .send(newExpenseTestObj)
-      .expect(500);
-  });
-
-  it('GET /expense returns 500 if theres a prob connecting to db', async () => {
-    const res = await request(app)
-      .post('/user/login')
-      .send(testLoginUser)
-      .expect(200);
-    const cookie = res.headers['set-cookie'];
-
-    poolSpy.mockImplementation(() => {
-      throw new Error('PostgreSQL database error: Connection refused');
-    });
-
-    await request(app)
-      .get('/expense')
-      .set('Cookie', cookie)
-      .send(newExpenseTestObj)
-      .expect(500);
-  });
-});
-
-describe('GET /expense', () => {
-  it('returns a 401 if not logged in', async () => {
-    await request(app).get('/expense').expect(401);
-  });
-
-  it('returns 200 with a message if the user has no expenses entered', async () => {
-    const agent = request.agent(app);
-    await agent.post('/user/login').send(getNoExpenseTestUser).expect(200);
-
-    const res = await agent.get('/expense').expect(200);
-    expect(res.body).toBe('Start by entering an expense.');
-  });
-
-  it('returns 200 and a list of expenses for the current user', async () => {
-    const agent = request.agent(app);
-    await agent.post('/user/login').send(getExpenseTestUser).expect(200);
-
-    const res = await agent.get('/expense').expect(200);
-    expect(res.body.length).toBe(2);
-    expect(res.body[0].user_email).toBe(getExpenseTestUser.email);
-  });
-});
-
-describe('POST /expense', () => {
+describe('expense routes', () => {
   afterAll(async () => {
     await pool.end();
   });
 
-  describe('invalid form data', () => {
-    let agent;
+  describe('check error handling for db calls', () => {
+    let poolSpy;
 
-    beforeAll(async () => {
-      agent = request.agent(app);
+    beforeEach(() => {
+      poolSpy = jest.spyOn(pool, 'query');
+    });
+
+    afterEach(() => {
+      poolSpy.mockRestore();
+    });
+
+    it('POST /expense returns 500 if theres a prob connecting to db', async () => {
+      const res = await request(app)
+        .post('/user/login')
+        .send(testLoginUser)
+        .expect(200);
+      const cookie = res.headers['set-cookie'];
+
+      poolSpy.mockImplementation(() => {
+        throw new Error('PostgreSQL database error: Connection refused');
+      });
+
+      await request(app)
+        .post('/expense')
+        .set('Cookie', cookie)
+        .send(newExpenseTestObj)
+        .expect(500);
+    });
+
+    it('GET /expense returns 500 if theres a prob connecting to db', async () => {
+      const res = await request(app)
+        .post('/user/login')
+        .send(testLoginUser)
+        .expect(200);
+      const cookie = res.headers['set-cookie'];
+
+      poolSpy.mockImplementation(() => {
+        throw new Error('PostgreSQL database error: Connection refused');
+      });
+
+      await request(app)
+        .get('/expense')
+        .set('Cookie', cookie)
+        .send(newExpenseTestObj)
+        .expect(500);
+    });
+  });
+
+  describe('GET /expense', () => {
+    it('returns a 401 if not logged in', async () => {
+      await request(app).get('/expense').expect(401);
+    });
+
+    it('returns 200 with a message if the user has no expenses entered', async () => {
+      const agent = request.agent(app);
+      await agent.post('/user/login').send(getNoExpenseTestUser).expect(200);
+
+      const res = await agent.get('/expense').expect(200);
+      expect(res.body).toBe('Start by entering an expense.');
+    });
+
+    it('returns 200 and a list of expenses for the current user', async () => {
+      const agent = request.agent(app);
+      await agent.post('/user/login').send(getExpenseTestUser).expect(200);
+
+      const res = await agent.get('/expense').expect(200);
+      expect(res.body.length).toBe(2);
+      expect(res.body[0].user_email).toBe(getExpenseTestUser.email);
+    });
+  });
+
+  describe('POST /expense', () => {
+    describe('invalid form data', () => {
+      let agent;
+
+      beforeAll(async () => {
+        agent = request.agent(app);
+        await agent.post('/user/login').send(goodTestUser).expect(200);
+      });
+
+      it('returns 400 if given invalid date data', async () => {
+        const res = await agent
+          .post('/expense')
+          .set('Accept', 'application/json')
+          .send(badDateExpenseTestObj);
+        checkValidation(res);
+      });
+
+      it('returns 400 if given invalid amount data', async () => {
+        const res = await agent
+          .post('/expense')
+          .set('Accept', 'application/json')
+          .send(badAmountExpenseTestObj);
+        checkValidation(res);
+      });
+
+      it('returns 400 if given invalid account from data', async () => {
+        const res = await agent
+          .post('/expense')
+          .set('Accept', 'application/json')
+          .send(badAcctFromExpenseTestObj);
+        checkValidation(res);
+      });
+
+      it('returns 400 if given invalid category id data', async () => {
+        const res = await agent
+          .post('/expense')
+          .set('Accept', 'application/json')
+          .send(badCategoryExpenseTestObj);
+        checkValidation(res);
+      });
+
+      it('returns 400 if given invalid paid to data', async () => {
+        const res = await agent
+          .post('/expense')
+          .set('Accept', 'application/json')
+          .send(badPaidToExpenseTestObj);
+        checkValidation(res);
+      });
+    });
+
+    it('returns 401 if the user is not logged in', async () => {
+      await request(app).post('/expense').expect(401);
+    });
+
+    it('returns 201 and the created expense_id', async () => {
+      // login
+      const agent = request.agent(app);
       await agent.post('/user/login').send(goodTestUser).expect(200);
-    });
-
-    it('returns 400 if given invalid date data', async () => {
+      // POST new expense
       const res = await agent
         .post('/expense')
-        .set('Accept', 'application/json')
-        .send(badDateExpenseTestObj);
-      checkValidation(res);
+        .send(newExpenseTestObj)
+        .expect(201);
+      expect(Number.isInteger(res.body)).toBe(true);
+      // for cleanup
+      const newExpId = res.body;
+      // cleanup
+      await pool.query('DELETE FROM expenses WHERE expense_id = $1', [
+        newExpId,
+      ]);
+      const cleanupRes = await pool.query(
+        'SELECT * FROM expenses WHERE expense_id = $1',
+        [newExpId],
+      );
+      // test cleanup was ok
+      expect(cleanupRes.rowCount).toBe(0);
     });
-
-    it('returns 400 if given invalid amount data', async () => {
-      const res = await agent
-        .post('/expense')
-        .set('Accept', 'application/json')
-        .send(badAmountExpenseTestObj);
-      checkValidation(res);
-    });
-
-    it('returns 400 if given invalid account from data', async () => {
-      const res = await agent
-        .post('/expense')
-        .set('Accept', 'application/json')
-        .send(badAcctFromExpenseTestObj);
-      checkValidation(res);
-    });
-
-    it('returns 400 if given invalid category id data', async () => {
-      const res = await agent
-        .post('/expense')
-        .set('Accept', 'application/json')
-        .send(badCategoryExpenseTestObj);
-      checkValidation(res);
-    });
-
-    it('returns 400 if given invalid paid to data', async () => {
-      const res = await agent
-        .post('/expense')
-        .set('Accept', 'application/json')
-        .send(badPaidToExpenseTestObj);
-      checkValidation(res);
-    });
-  });
-
-  it('returns 401 if the user is not logged in', async () => {
-    await request(app).post('/expense').expect(401);
-  });
-
-  it('returns 201 and the created expense_id', async () => {
-    // login
-    const agent = request.agent(app);
-    await agent.post('/user/login').send(goodTestUser).expect(200);
-    // POST new expense
-    const res = await agent
-      .post('/expense')
-      .send(newExpenseTestObj)
-      .expect(201);
-    expect(Number.isInteger(res.body)).toBe(true);
-    // for cleanup
-    const newExpId = res.body;
-    // cleanup
-    await pool.query('DELETE FROM expenses WHERE expense_id = $1', [newExpId]);
-    const cleanupRes = await pool.query(
-      'SELECT * FROM expenses WHERE expense_id = $1',
-      [newExpId],
-    );
-    // test cleanup was ok
-    expect(cleanupRes.rowCount).toBe(0);
   });
 });
--- backend/routes/userRoutes.test.js
+++ backend/routes/userRoutes.test.js
@@ -17,266 +17,274 @@ const {
   userToDelete,
 } = require('../userTestResources');
 
-describe('POST /user', () => {
-  describe('invalid form data', () => {
-    it('returns a 400 if email is invalid', async () => {
-      const res = await request(app)
-        .post('/user')
-        .send(badEmailUser)
-        .set('Accept', 'application/json');
-      checkValidation(res);
-    });
-
-    it('returns a 400 if password is invalid', async () => {
-      const res = await request(app)
-        .post('/user')
-        .send(badPwUser)
-        .set('Accept', 'application/json');
-      checkValidation(res);
-    });
-
-    it('returns a 400 if passwordConf is invalid', async () => {
-      const res = await request(app)
-        .post('/user')
-        .send(badPwConfUser)
-        .set('Accept', 'application/json');
-      checkValidation(res);
-    });
+describe('user routes', () => {
+  afterAll(async () => {
+    await pool.end();
   });
 
-  describe('creates a user account', () => {
-    it('creates a user account and returns a 201 with the users new id', async () => {
-      // create the account
-      const res = await request(app).post('/user').send(goodUser).expect(201);
-      // store the user_id for cleanup
-      const user_id = res.body;
-      expect(Number.isInteger(user_id)).toBe(true);
-      // cleanup - delete user account and test
-      await pool.query('DELETE FROM users WHERE user_id = $1', [user_id]);
-      const cleanupRes = await pool.query(
-        'SELECT FROM users WHERE user_id = $1',
-        [user_id],
-      );
-      // test the cleanup
-      expect(cleanupRes.rowCount).toBe(0);
-    });
-
-    it('returns a 500 error if email is already taken', async () => {
-      const res = await request(app)
-        .post('/user')
-        .send(duplicateEmailUser)
-        .expect(500);
-
-      expect(res.body).toBe(
-        'error: duplicate key value violates unique constraint "users_user_email_key"',
-      );
-    });
-  });
-});
-
-describe('POST /user/login', () => {
-  describe('invalid form data', () => {
-    it('returns a 400 if email is invalid', async () => {
-      const res = await request(app)
-        .post('/user/login')
-        .send({ email: badEmailUser.email, password: badEmailUser.password })
-        .set('Accept', 'application/json');
-      checkValidation(res);
-    });
-
-    it('returns a 400 if password is invalid', async () => {
-      const res = await request(app)
-        .post('/user/login')
-        .send({ email: badPwUser.email, password: badPwUser.password })
-        .set('Accept', 'application/json');
-      checkValidation(res);
-    });
-  });
-
-  it('returns 200 and a session cookie if given good credentials', async () => {
-    const res = await request(app)
-      .post('/user/login')
-      .send(testLoginUser)
-      .expect(200);
-    expect(res.headers['set-cookie']).toBeDefined();
-    expect(res.headers['set-cookie'][0]).toMatch(/connect.sid=/);
-  });
-
-  it('returns 400 if given bad credentials', async () => {
-    await request(app).post('/user/login').send(testBadLoginPwUser).expect(400);
-  });
-
-  it('returns 404 if given nonexistant user', async () => {
-    await request(app)
-      .post('/user/login')
-      .send(testBadLoginEmailUser)
-      .expect(404);
-  });
-});
-
-describe('GET /user', () => {
-  it('returns 401 if no is logged in', async () => {
-    const res = await request(app).get('/user').expect(401);
-    expect(res.body).toBe('There is no one currently logged in.');
-  });
-
-  it('returns 200 and the current user_email', async () => {
-    // login a user
-    const agent = request.agent(app);
-    await agent.post('/user/login').send(testLoginUser).expect(200);
-    // get the user
-    const res = await agent.get('/user').expect(200);
-    expect(res.body.user_email).toBe(testLoginUser.email);
-  });
-});
-
-describe('GET /user/logout', () => {
-  it('returns 401 if there is not a user logged in', async () => {
-    await request(app).get('/user/logout').expect(401);
-  });
-
-  it('returns 200 with a msg if a user is logged in', async () => {
-    const agent = request.agent(app);
-    await agent.post('/user/login').send(testLoginUser).expect(200);
-    const res = await agent.get('/user/logout').expect(200);
-    expect(res.body).toBe('The user is now logged out.');
-  });
-
-  it('returns a 500 if there is a problem logging out', async () => {
-    const errMsg = 'There was a problem logging out.';
-    // mock the session destruction to trigger a 500
-    jest
-      .spyOn(session.Session.prototype, 'destroy')
-      .mockImplementationOnce((cb) => {
-        cb(new Error(errMsg));
+  describe('POST /user', () => {
+    describe('invalid form data', () => {
+      it('returns a 400 if email is invalid', async () => {
+        const res = await request(app)
+          .post('/user')
+          .send(badEmailUser)
+          .set('Accept', 'application/json');
+        checkValidation(res);
       });
 
-    const agent = request.agent(app);
-    // login first
-    await agent.post('/user/login').send(testLoginUser).expect(200);
-    const logoutRes = await agent.get('/user/logout').expect(500);
-    expect(logoutRes.body).toBe(errMsg);
-    expect(session.Session.prototype.destroy).toHaveBeenCalled();
+      it('returns a 400 if password is invalid', async () => {
+        const res = await request(app)
+          .post('/user')
+          .send(badPwUser)
+          .set('Accept', 'application/json');
+        checkValidation(res);
+      });
 
-    jest.restoreAllMocks();
+      it('returns a 400 if passwordConf is invalid', async () => {
+        const res = await request(app)
+          .post('/user')
+          .send(badPwConfUser)
+          .set('Accept', 'application/json');
+        checkValidation(res);
+      });
+    });
+
+    describe('creates a user account', () => {
+      it('creates a user account and returns a 201 with the users new id', async () => {
+        // create the account
+        const res = await request(app).post('/user').send(goodUser).expect(201);
+        // store the user_id for cleanup
+        const user_id = res.body;
+        expect(Number.isInteger(user_id)).toBe(true);
+        // cleanup - delete user account and test
+        await pool.query('DELETE FROM users WHERE user_id = $1', [user_id]);
+        const cleanupRes = await pool.query(
+          'SELECT FROM users WHERE user_id = $1',
+          [user_id],
+        );
+        // test the cleanup
+        expect(cleanupRes.rowCount).toBe(0);
+      });
+
+      it('returns a 500 error if email is already taken', async () => {
+        const res = await request(app)
+          .post('/user')
+          .send(duplicateEmailUser)
+          .expect(500);
+
+        expect(res.body).toBe(
+          'error: duplicate key value violates unique constraint "users_user_email_key"',
+        );
+      });
+    });
   });
-});
 
-describe('PUT /user', () => {
-  describe('invalid form data', () => {
-    let agent;
+  describe('POST /user/login', () => {
+    describe('invalid form data', () => {
+      it('returns a 400 if email is invalid', async () => {
+        const res = await request(app)
+          .post('/user/login')
+          .send({ email: badEmailUser.email, password: badEmailUser.password })
+          .set('Accept', 'application/json');
+        checkValidation(res);
+      });
 
-    beforeEach(async () => {
-      agent = request.agent(app);
-      await agent.post('/user/login').send(testLoginUser).expect(200);
+      it('returns a 400 if password is invalid', async () => {
+        const res = await request(app)
+          .post('/user/login')
+          .send({ email: badPwUser.email, password: badPwUser.password })
+          .set('Accept', 'application/json');
+        checkValidation(res);
+      });
     });
 
-    it('returns a 400 if given an invalid email', async () => {
-      const res = await agent
-        .put('/user')
-        .send(badEmailUser)
-        .set('Accept', 'application/json');
-      checkValidation(res);
-    });
-
-    it('returns a 400 if given an invalid password', async () => {
-      const res = await agent
-        .put('/user')
-        .send(badPwUser)
-        .set('Accept', 'application/json');
-      checkValidation(res);
-    });
-
-    it('returns a 400 if given an invalid password conf', async () => {
-      const res = await agent
-        .put('/user')
-        .send(badPwConfUser)
-        .set('Accept', 'application/json');
-      checkValidation(res);
-    });
-  });
-
-  describe('updates user info', () => {
-    it('returns a 200 and updates user data', async () => {
-      // login the user
-      const agent = request.agent(app);
-      await agent.post('/user/login').send(resetUpdatedUser).expect(200);
-      // update the user
-      const updRes = await agent.put('/user').send(updatedUser).expect(200);
-      expect(updRes.body).toBe('User info has been updated.');
-      // logout the user
-      await agent.get('/user/logout').expect(200);
-      // login the updated user
-      await agent.post('/user/login').send(updatedUser).expect(200);
-      // **cleanup**
-      // change the user info back
-      const cleanupRes = await agent
-        .put('/user')
-        .send(resetUpdatedUser)
-        .expect(200);
-      expect(cleanupRes.body).toBe('User info has been updated.');
-    });
-
-    it('returns a 500 if provided email is taken', async () => {
-      const agent = request.agent(app);
-      await agent.post('/user/login').send(resetUpdatedUser).expect(200);
-      await agent.put('/user').send(duplicateEmailUser).expect(500);
-    });
-
-    it('returns 401 if there is not a user logged in', async () => {
+    it('returns 200 and a session cookie if given good credentials', async () => {
       const res = await request(app)
-        .put('/user')
+        .post('/user/login')
         .send(testLoginUser)
-        .expect(401);
+        .expect(200);
+      expect(res.headers['set-cookie']).toBeDefined();
+      expect(res.headers['set-cookie'][0]).toMatch(/connect.sid=/);
+    });
+
+    it('returns 400 if given bad credentials', async () => {
+      await request(app)
+        .post('/user/login')
+        .send(testBadLoginPwUser)
+        .expect(400);
+    });
+
+    it('returns 404 if given nonexistant user', async () => {
+      await request(app)
+        .post('/user/login')
+        .send(testBadLoginEmailUser)
+        .expect(404);
+    });
+  });
+
+  describe('GET /user', () => {
+    it('returns 401 if no is logged in', async () => {
+      const res = await request(app).get('/user').expect(401);
       expect(res.body).toBe('There is no one currently logged in.');
     });
-  });
-});
 
-describe('DELETE /user', () => {
-  it('returns 401 if not logged in', async () => {
-    await request(app).delete('/user').expect(401);
-  });
-
-  it('returns 204 after deleting user', async () => {
-    // create a user to delete
-    await request(app).post('/user').send(userToDelete).expect(201);
-    // login the new user
-    const agent = request.agent(app);
-    await agent.post('/user/login').send(userToDelete).expect(200);
-    // delete the user
-    await agent.delete('/user').expect(204);
-  });
-});
-
-describe('check error handling if the db call is bad', () => {
-  // mock the db call to force an error from the server
-  let poolSpy;
-
-  beforeEach(() => {
-    poolSpy = jest.spyOn(pool, 'query');
-  });
-
-  afterEach(() => {
-    poolSpy.mockRestore();
-  });
-
-  it('DELETE /user returns 500 if there is a prob calling the database', async () => {
-    const res = await request(app)
-      .post('/user/login')
-      .send(testLoginUser)
-      .expect(200);
-    expect(res.headers['set-cookie']).toBeDefined();
-    const cookie = res.headers['set-cookie'];
-    poolSpy.mockImplementation(() => {
-      throw new Error('PostgreSQL database error: Connection refused');
+    it('returns 200 and the current user_email', async () => {
+      // login a user
+      const agent = request.agent(app);
+      await agent.post('/user/login').send(testLoginUser).expect(200);
+      // get the user
+      const res = await agent.get('/user').expect(200);
+      expect(res.body.user_email).toBe(testLoginUser.email);
     });
-    await request(app).delete('/user').set('Cookie', cookie).expect(500);
   });
 
-  it('POST /user/login returns 500 if there is a prob calling the database', async () => {
-    poolSpy.mockImplementation(() => null);
-    await request(app).post('/user/login').send(testLoginUser).expect(500);
-    pool.end();
+  describe('GET /user/logout', () => {
+    it('returns 401 if there is not a user logged in', async () => {
+      await request(app).get('/user/logout').expect(401);
+    });
+
+    it('returns 200 with a msg if a user is logged in', async () => {
+      const agent = request.agent(app);
+      await agent.post('/user/login').send(testLoginUser).expect(200);
+      const res = await agent.get('/user/logout').expect(200);
+      expect(res.body).toBe('The user is now logged out.');
+    });
+
+    it('returns a 500 if there is a problem logging out', async () => {
+      const errMsg = 'There was a problem logging out.';
+      // mock the session destruction to trigger a 500
+      jest
+        .spyOn(session.Session.prototype, 'destroy')
+        .mockImplementationOnce((cb) => {
+          cb(new Error(errMsg));
+        });
+
+      const agent = request.agent(app);
+      // login first
+      await agent.post('/user/login').send(testLoginUser).expect(200);
+      const logoutRes = await agent.get('/user/logout').expect(500);
+      expect(logoutRes.body).toBe(errMsg);
+      expect(session.Session.prototype.destroy).toHaveBeenCalled();
+
+      jest.restoreAllMocks();
+    });
+  });
+
+  describe('PUT /user', () => {
+    describe('invalid form data', () => {
+      let agent;
+
+      beforeEach(async () => {
+        agent = request.agent(app);
+        await agent.post('/user/login').send(testLoginUser).expect(200);
+      });
+
+      it('returns a 400 if given an invalid email', async () => {
+        const res = await agent
+          .put('/user')
+          .send(badEmailUser)
+          .set('Accept', 'application/json');
+        checkValidation(res);
+      });
+
+      it('returns a 400 if given an invalid password', async () => {
+        const res = await agent
+          .put('/user')
+          .send(badPwUser)
+          .set('Accept', 'application/json');
+        checkValidation(res);
+      });
+
+      it('returns a 400 if given an invalid password conf', async () => {
+        const res = await agent
+          .put('/user')
+          .send(badPwConfUser)
+          .set('Accept', 'application/json');
+        checkValidation(res);
+      });
+    });
+
+    describe('updates user info', () => {
+      it('returns a 200 and updates user data', async () => {
+        // login the user
+        const agent = request.agent(app);
+        await agent.post('/user/login').send(resetUpdatedUser).expect(200);
+        // update the user
+        const updRes = await agent.put('/user').send(updatedUser).expect(200);
+        expect(updRes.body).toBe('User info has been updated.');
+        // logout the user
+        await agent.get('/user/logout').expect(200);
+        // login the updated user
+        await agent.post('/user/login').send(updatedUser).expect(200);
+        // **cleanup**
+        // change the user info back
+        const cleanupRes = await agent
+          .put('/user')
+          .send(resetUpdatedUser)
+          .expect(200);
+        expect(cleanupRes.body).toBe('User info has been updated.');
+      });
+
+      it('returns a 500 if provided email is taken', async () => {
+        const agent = request.agent(app);
+        await agent.post('/user/login').send(resetUpdatedUser).expect(200);
+        await agent.put('/user').send(duplicateEmailUser).expect(500);
+      });
+
+      it('returns 401 if there is not a user logged in', async () => {
+        const res = await request(app)
+          .put('/user')
+          .send(testLoginUser)
+          .expect(401);
+        expect(res.body).toBe('There is no one currently logged in.');
+      });
+    });
+  });
+
+  describe('DELETE /user', () => {
+    it('returns 401 if not logged in', async () => {
+      await request(app).delete('/user').expect(401);
+    });
+
+    it('returns 204 after deleting user', async () => {
+      // create a user to delete
+      await request(app).post('/user').send(userToDelete).expect(201);
+      // login the new user
+      const agent = request.agent(app);
+      await agent.post('/user/login').send(userToDelete).expect(200);
+      // delete the user
+      await agent.delete('/user').expect(204);
+    });
+  });
+
+  describe('check error handling if the db call is bad', () => {
+    // mock the db call to force an error from the server
+    let poolSpy;
+
+    beforeEach(() => {
+      poolSpy = jest.spyOn(pool, 'query');
+    });
+
+    afterEach(() => {
+      poolSpy.mockRestore();
+    });
+
+    it('DELETE /user returns 500 if there is a prob calling the database', async () => {
+      const res = await request(app)
+        .post('/user/login')
+        .send(testLoginUser)
+        .expect(200);
+      expect(res.headers['set-cookie']).toBeDefined();
+      const cookie = res.headers['set-cookie'];
+      poolSpy.mockImplementation(() => {
+        throw new Error('PostgreSQL database error: Connection refused');
+      });
+      await request(app).delete('/user').set('Cookie', cookie).expect(500);
+    });
+
+    it('POST /user/login returns 500 if there is a prob calling the database', async () => {
+      poolSpy.mockImplementation(() => null);
+      await request(app).post('/user/login').send(testLoginUser).expect(500);
+    });
   });
 });
