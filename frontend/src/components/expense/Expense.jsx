66ed9720232f4de134d6f66f20d4a324eb605ab6
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-11-25 17:00:37 -0500

    converted the expenses to details elements and started to style

--- frontend/src/components/expense/Expense.jsx
+++ frontend/src/components/expense/Expense.jsx
@@ -1,5 +1,6 @@
 import EditExpense from "../edit_expense/EditExpense";
 import DeleteExpense from "../delete_expense/DeleteExpense";
+import styles from "./Expense.module.css";
 
 function Expense({
   expense_id,
@@ -15,14 +16,18 @@ function Expense({
   setExpIdToEdit,
 }) {
   return (
-    <div data-exp-id={expense_id}>
-      {/* need to make this more presentable*/}
-      <p>Expense Id: {expense_id}</p>
-      <p>Expense Date:{expense_date.split("T")[0]}</p>
-      <p>Expense Amount: {expense_amount}</p>
+    <details
+      name='expense'
+      data-exp-id={expense_id}
+    >
+      <summary>
+        On {expense_date.split("T")[0]} ${expense_amount} was paid to {paid_to}
+      </summary>
+      {/* <p>Expense Date:{expense_date.split("T")[0]}</p> */}
+      {/* <p>Expense Amount: {expense_amount}</p> */}
       <p>Account Paid From: {account_paid_from}</p>
       <p>Category: {categoryIdToStr(category_id)}</p>
-      <p>Paid To: {paid_to}</p>
+      {/* <p>Paid To: {paid_to}</p> */}
       <p>Notes: {notes}</p>
       <EditExpense
         expense_id={expense_id}
@@ -30,7 +35,7 @@ function Expense({
         setExpIdToEdit={setExpIdToEdit}
       />
       <DeleteExpense setExpTrigger={setExpTrigger} />
-    </div>
+    </details>
   );
 }
 
