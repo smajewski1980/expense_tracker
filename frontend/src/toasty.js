8fe95ae3e5215b6afb32df4155674abf3edfeec6
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-12-01 20:04:58 -0500

    adj some toastify styles

--- frontend/src/toasty.js
+++ frontend/src/toasty.js
@@ -2,7 +2,6 @@ export default function toasty(message) {
   Toastify({
     text: message,
     duration: 3000,
-    destination: "https://github.com/apvarun/toastify-js",
     newWindow: true,
     close: true,
     gravity: "top", // `top` or `bottom`
@@ -10,7 +9,9 @@ export default function toasty(message) {
     stopOnFocus: true, // Prevents dismissing of toast on hover
     style: {
       background:
-        "linear-gradient(to right, rgb(51, 65, 0), rgba(135, 145, 99, 1)",
+        "linear-gradient(to top, rgba(194, 91, 78, 1), rgba(131, 60, 60, 1)",
+      padding: ".5rem .75rem .5rem 2.25rem",
+      textIndent: "-1.5rem",
     },
     onClick: function () {}, // Callback after click
   }).showToast();
